8b606b3cbe98f57700747d2083e3cb3a73206657
Erick Rodriguez <erickrod54@gmail.com>
2022-08-08 10:30:51 -0400

    Serveless functions version 5 - 1-hello js functions - Features: Testing Bad request statusCode 400

--- functions/1-hello.js
+++ functions/1-hello.js
@@ -1,21 +1,20 @@
 /**to test it */
 // domain /.netlify/functions/1-hello
 
-/** Serveless functions version 4 - 1-hello js function
+/** Serveless functions version 5 - 1-hello js function
  * - Features: 
  * 
- *          --> Testing 'error' statusCode 404 
+ *          --> Testing 'Bad request' statusCode 400 
  * 
- * Note: the 'body' prop always need to be a string that's
- * why the implementation of 'JSON.stringify' is necessary,
- * also is widely use to build customs API's
- * 
- * The status code depends on the 'request' that im going
- * to build, for the first example:
+ * Note: the status code can generate a different object
+ * or a different 'statusText' prop as i was testing
+ * in this and previous versions, axios library has this 
+ * object to help us to build the back-end:
  * 
  *                 statusCode: 200 --> success request
  * 
  *  other can be: statusCode: 404 --> error status code
+ *                            400 --> Bad request                  
  * 
  *  full reference:
  * 
@@ -28,10 +27,9 @@
 exports.handler = async(event, context, cb) => {
 
        return{
-        /**here i returned but i have to apply
-         * 'JSON.stringify' in order to visualize 
-         * as an API in the browser*/
-        statusCode: 404,
+        /**when i change the 'statusCode' it changes
+         * the 'statusText' prop message*/
+        statusCode: 400,
         body:'Resource not found',
     }
     
