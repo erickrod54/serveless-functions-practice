ccedf53538610321f74b7cbc80c261be8390888e
Erick Rodriguez <erickrod54@gmail.com>
2022-08-09 12:12:53 -0400

    Serveless fucntions version 7 - 1-hello > app js file - Features: Changing the url path to get the request from the new url > api/1-hello

--- examples/1-hello/app.js
+++ examples/1-hello/app.js
@@ -1,35 +1,12 @@
 //const { default: axios } = require('axios')
 
-/** Serveless functions version 6 - 1-hello > app js file
+/** Serveless functions version 7 - 1-hello > app js file
  * - Features: 
  * 
- *          --> Testing 200 statusCode by redirecting
- *              the domain on 'toml' file
+ *          --> Changing the url path to get the request
+ *              from the new url > /api/1-hello
  * 
- *          --> Converting from '200' and '301' requests
- *              by setting it to '200' on 'toml' file.
- * 
- * Note: for this version im going to build a friendly 
- * shortcut for the domain
- *  
- *   from this ==> domain /.netlify/functions/1-hello
- *   to this   ==> domain /api/1-hello
- * 
- * all these changes are made on the netlify.toml  
- * this is called 'redirects' ( this is a netlify feature)
- * checking the star '*' and ':splat' they will match
- * 
- * adding to the 'toml' this las two lines:
- *  
- *   [[redirects]]
- *      
- *      from = '/api/*'
- *      to = '/.netlify/functions/:splat'
- * 
- * and checking the 'chrome > network' tab will generate
- * two request '200' and '301' so i add a last line
- *   
- *     status = 200
+ * Note: Now this new url can be use to build the front-end
  */
 
 /**here i select where i'll render the content
@@ -39,11 +16,10 @@ const result = document.querySelector('.result');
 /**here i fetch the data from the url test api */
 const fetchData = async () => {
     try {
-        const request = await axios.get('/.netlify/functions/1-hello')
-        /**then i get the request prop - log it */
-        console.log('this is the request i got  ==>', request)
+        
+
         /**then i pull the 'data' prop - log it*/
-        const { data } = await axios.get('/.netlify/functions/1-hello')
+        const { data } = await axios.get('/api/1-hello')
         console.log('this is the data i got  ==>', data)
 
         /**then i i just add to my 'textContent' and 
