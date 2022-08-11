1246c96ebe4a9eb431f92ba4e60db21f0cd8566b
Erick Rodriguez <erickrod54@gmail.com>
2022-08-11 00:13:16 -0400

    Serveless functions version 8 - 2-basi-api js functions - Features: Adding headers in order to allow the communication with urlApiDeployed

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -1,21 +1,16 @@
-/** Serveless functions version 7 - 2-basic-api js function
+/** Serveless functions version 8 - 2-basic-api js function
  * - Features: 
  * 
- *          --> Building the basic api js function.
+ *          --> Adding 'headers' in order to allow
+ *              the communication with the 
+ *              'urlApiDeployed'
  * 
- *          --> Testing 'sucess' statusCode 200 
- *             with the 'items' require the 'data'
- *             location. 
- * 
- * Note: By this version using assets i locate all resources
- * as well an object that gatter all the assets information
- * in a 'data' js file, so doing 'require' syntax and the
- * 'JSON.stringify' over the items i can get nicely the 
- * JSON object from the api ( by this version with the 
- * link > http://localhost:8888/api/2-basic-api )
- * 
- * Reference to '/assets/data' to look in detail how is been 
- * the data built
+ * Note: the 'headers' is a must if i want my API to be
+ * accesible from different applications, as i have two
+ * functions '1-hello' and '2-basic' api by the moment i'm
+ * adding it on '2-basic-api', but this must be add to every
+ * function that holds an api and i want them to cummunicate
+ * with different apps
  */
 
 const items = require('../assets/data')
@@ -23,8 +18,9 @@ const items = require('../assets/data')
 exports.handler = async(event, context, cb) => {
 
     return{
-     /**when i change the 'statusCode' it changes
-      * the 'statusText' prop message*/
+     headers:{
+        'Acess-Control-Allow-Origin' : '*',
+     },
      statusCode: 200,
      body: JSON.stringify(items),
  }
